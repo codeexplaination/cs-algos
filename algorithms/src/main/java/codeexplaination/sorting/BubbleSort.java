dd1ff8d97c00a73f248089815e43d52bc60b2989
Saurabh Jain <jainsaurabh86@gmail.com>
2021-01-01 14:59:35 -0500

    Added bubble sort algorithm

---
+++ algorithms/src/main/java/codeexplaination/sorting/BubbleSort.java
@@ -0,0 +1,41 @@
+package codeexplaination.sorting;
+
+import java.util.Arrays;
+
+/**
+ * Class explaining bubble sort algorithm
+ *
+ */
+public class BubbleSort
+{
+    public static void sort(int[] input){
+
+        int size = input.length;
+
+        printHelper("Input array is: ", input);
+        for(int i=0; i<size; i++){
+            for (int j=0; j<size; j++){
+                if(input[i] > input[j]){
+                    int temp = input[i];
+                    input[i] = input[j];
+                    input[j] = temp;
+                }
+            }
+        }
+        printHelper("Sorted array is: ", input);
+    }
+
+    private static void printHelper(String s, int[] input) {
+        System.out.print(s);
+        for(int i : input){
+            System.out.print(" " + input[i]);
+        }
+        System.out.println("");
+    }
+
+    public static void main( String[] args )
+    {
+        int[] input = {2, 4, 3, -1, 8, 5};
+        sort(input);
+    }
+}
