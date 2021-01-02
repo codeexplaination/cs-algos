90efcade2b3bd498dc916827468c5e499fb87946
Saurabh Jain <jainsaurabh86@gmail.com>
2021-01-02 11:50:22 -0500

    Added implementation for tree bfs and dfs

--- algorithms/src/main/java/codeexplaination/trees/traversal/BFS.java
+++ algorithms/src/main/java/codeexplaination/trees/traversal/BFS.java
@@ -1,7 +1,96 @@
 package codeexplaination.trees.traversal;
 
+/**
+ * This is also called as level order traversal
+ */
 public class BFS {
-    public static void main(String args[]){
-        // TODO: Complete this code
+    private class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    Node root;
+
+    public void insert(int data) {
+        root = insertRec(root, data);
+    }
+
+    private Node insertRec(Node root, int data) {
+        if (root == null) {
+            root = new Node(data);
+        }
+
+        if (data < root.data) {
+            root.left = insertRec(root.left, data);
+        } else if (data > root.data) {
+            root.right = insertRec(root.right, data);
+        }
+
+        return root;
+    }
+
+    public int getHeight(Node root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = getHeight(root.left);
+            int rightHeight = getHeight(root.right);
+
+            if (leftHeight > rightHeight) {
+                return leftHeight + 1;
+            } else {
+                return rightHeight + 1;
+            }
+        }
+    }
+
+    public void printBFS() {
+        int height = getHeight(root);
+        for (int i = 1; i <= height; i++) {
+            printLevelOrder(root, i);
+        }
+    }
+
+    private void printLevelOrder(Node root, int level) {
+        if (root == null) {
+            return;
+        }
+
+        if (level == 1) {
+            System.out.print(root.data + " ");
+        } else if (level > 1) {
+            printLevelOrder(root.left, level - 1);
+            printLevelOrder(root.right, level - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        BFS bfs = new BFS();
+        bfs.insert(50);
+        bfs.insert(30);
+        bfs.insert(20);
+        bfs.insert(35);
+        bfs.insert(100);
+        bfs.insert(110);
+        bfs.insert(90);
+        bfs.insert(95);
+        bfs.insert(53);
+        bfs.insert(12);
+        bfs.insert(22);
+
+        //                    50
+        //           30                  100
+        //      20       35         90         110
+        //   12     22           53     95
+
+        System.out.println("Height of the tree is: " + bfs.getHeight(bfs.root));
+        bfs.printBFS();
     }
 }
--- algorithms/src/main/java/codeexplaination/trees/traversal/DFS.java
+++ algorithms/src/main/java/codeexplaination/trees/traversal/DFS.java
@@ -1,7 +1,105 @@
 package codeexplaination.trees.traversal;
 
+/**
+ * DFS can be either inorder(LVR), preorder(VLR) or postorder(LRV) traversal
+ */
 public class DFS {
-    public static void main(String args[]){
-        // TODO: Complete this code
+    private class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    Node root;
+
+    public void insert(int data) {
+        root = insertRec(root, data);
+    }
+
+    private Node insertRec(Node root, int data) {
+        if (root == null) {
+            root = new Node(data);
+        }
+
+        if (data < root.data) {
+            root.left = insertRec(root.left, data);
+        } else if (data > root.data) {
+            root.right = insertRec(root.right, data);
+        }
+
+        return root;
+    }
+
+    public void printPostorder() {
+        System.out.println("Postorder traversal using recursion is: ");
+        postorderUsingRecursion(root);
+    }
+
+    private void postorderUsingRecursion(Node root) {
+        if (root != null) {
+            postorderUsingRecursion(root.left);
+            postorderUsingRecursion(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+
+    public void printInorder() {
+        System.out.println("Inorder traversal using recursion is: ");
+        inorderUsingRecursion(root);
+    }
+
+    private void inorderUsingRecursion(Node root) {
+        if (root != null) {
+            inorderUsingRecursion(root.left);
+            System.out.print(root.data + " ");
+            inorderUsingRecursion(root.right);
+        }
+    }
+
+    public void printPreorder() {
+        System.out.println("Preorder traversal using recursion is: ");
+        preorderUsingRecursion(root);
+    }
+
+    private void preorderUsingRecursion(Node root) {
+        if (root != null) {
+            System.out.print(root.data + " ");
+            preorderUsingRecursion(root.left);
+            preorderUsingRecursion(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        DFS dfs = new DFS();
+        dfs.insert(50);
+        dfs.insert(30);
+        dfs.insert(20);
+        dfs.insert(35);
+        dfs.insert(100);
+        dfs.insert(110);
+        dfs.insert(90);
+        dfs.insert(95);
+        dfs.insert(53);
+        dfs.insert(12);
+        dfs.insert(22);
+
+        //                    50
+        //           30                  100
+        //      20       35         90         110
+        //   12     22           53     95
+
+        dfs.printInorder();
+        System.out.println();
+        System.out.println();
+        dfs.printPreorder();
+        System.out.println();
+        System.out.println();
+        dfs.printPostorder();
     }
 }
