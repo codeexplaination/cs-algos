7a40043a95cb3e923aff4233ec4190e2ecaf34ce
Saurabh Jain <jainsaurabh86@gmail.com>
2021-01-02 12:51:17 -0500

    Added implementation for single linked list

---
+++ algorithms/src/main/java/codeexplaination/linkedlist/DoubleLinkedList.java
@@ -0,0 +1,19 @@
+package codeexplaination.linkedlist;
+
+public class DoubleLinkedList {
+    private class Node {
+        int data;
+        Node next;
+        Node previous;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.previous = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        // TODO: Complete this code
+    }
+}
--- algorithms/src/main/java/codeexplaination/linkedlist/SingleLinkedList.java
+++ algorithms/src/main/java/codeexplaination/linkedlist/SingleLinkedList.java
@@ -1,7 +1,65 @@
 package codeexplaination.linkedlist;
 
 public class SingleLinkedList {
+    private class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+
+    public void push(int data) {
+        Node n = new Node(data);
+        n.next = head;
+        head = n;
+    }
+
+    public void printList() {
+        System.out.println();
+        System.out.print("Contents of linked list are: ");
+        Node n = head;
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+
+    public void reverse() {
+        Node current = head;
+        Node previous = null;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        head = previous;
+    }
+
     public static void main(String[] args) {
         // TODO: Complete this code
+        SingleLinkedList singleLinkedList = new SingleLinkedList();
+        singleLinkedList.push(10);
+        singleLinkedList.push(30);
+        singleLinkedList.push(20);
+        singleLinkedList.push(50);
+        singleLinkedList.push(35);
+        singleLinkedList.push(90);
+        singleLinkedList.push(15);
+
+        singleLinkedList.printList();
+
+        System.out.println();
+        System.out.println("Reversing the linked list now");
+        singleLinkedList.reverse();
+        singleLinkedList.printList();
     }
 }
