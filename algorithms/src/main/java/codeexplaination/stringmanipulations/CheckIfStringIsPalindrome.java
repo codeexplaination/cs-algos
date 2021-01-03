2853ab8521be4a252cc9eb9fb46ef4e05c643540
Saurabh Jain <jainsaurabh86@gmail.com>
2021-01-02 22:35:28 -0500

    Added logic for palindrome string

--- algorithms/src/main/java/codeexplaination/stringmanipulations/CheckIfStringIsPalindrome.java
+++ algorithms/src/main/java/codeexplaination/stringmanipulations/CheckIfStringIsPalindrome.java
@@ -1,7 +1,53 @@
 package codeexplaination.stringmanipulations;
 
 public class CheckIfStringIsPalindrome {
+    public static Boolean isPalindrome(String input) {
+        if (input != null) {
+            int start = 0;
+            int end = input.length() - 1;
+
+            while (start < end) {
+                if (input.charAt(start) != input.charAt(end)) {
+                    return false;
+                }
+
+                start++;
+                end--;
+            }
+
+        } else {
+            return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        // TODO: Complete this code
+        String input = "MADAM";
+        System.out.println("String " + input + " is palindrome=" + isPalindrome(input));
+
+        input = "MAD";
+        System.out.println("String " + input + " is palindrome=" + isPalindrome(input));
+
+        input = "KAYAK";
+        System.out.println("String " + input + " is palindrome=" + isPalindrome(input));
+
+        input = "CIVIC";
+        System.out.println("String " + input + " is palindrome=" + isPalindrome(input));
+
+        input = "";
+        System.out.println("String " + input + " is palindrome=" + isPalindrome(input));
+
+        input = null;
+        System.out.println("String " + input + " is palindrome=" + isPalindrome(input));
+
+        input = "LEVEL";
+        System.out.println("String " + input + " is palindrome=" + isPalindrome(input));
+
+        input = "LEEL";
+        System.out.println("String " + input + " is palindrome=" + isPalindrome(input));
+
+        input = "LEAD";
+        System.out.println("String " + input + " is palindrome=" + isPalindrome(input));
+
     }
 }
